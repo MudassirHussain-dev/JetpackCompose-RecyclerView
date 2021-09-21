8ae35b04a3bba3cc0a7c4c7cf36bfa8cca4a7ec9
AAM POWER DEVELOPER <AAM POWER DEVELOPER@DESKTOP-FLAPIQK>
2021-09-21 14:01:14 +0500

    Jetpack Compose Recyclerview basic learner

--- app/src/main/java/com/example/jetpackcomposestarting/data/retrofit/state/ApiState.kt
+++ app/src/main/java/com/example/jetpackcomposestarting/data/retrofit/state/ApiState.kt
@@ -8,10 +8,15 @@ sealed class ApiState {
     object Loading : ApiState()
     object Empty : ApiState()
 }
-/*
-sealed class ApiState<T>(val data: T? = null, val msg: Throwable? = null) {
+/*sealed class ApiState<T>(val data: T? = null, val msg: Throwable? = null) {
     class Success<T>(data: T? = null) : ApiState<T>(data = data)
     class Failure<T>(msg: Throwable) : ApiState<T>(msg = msg)
     class Loading<T> : ApiState<T>()
     class Empty<T> : ApiState<T>()
+}
+
+sealed class Response<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?) : Response<T>(data = data)
+    class Error<T>(message: String?, data: T? = null) : Response<T>(data = data, message = message)
+    class Loading<T>(data: T? = null) : Response<T>(data = data)
 }*/
